211da87851f9cbeffb9eb2a234242f97795ac88e
visertech12 <i.am.visertech@gmail.com>
2025-07-06 15:08:37 +0500

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+// main.jsx or index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);

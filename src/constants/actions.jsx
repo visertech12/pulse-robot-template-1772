04af1a782de5acdcf4e4dd83a07c736c7108b476
visertech12 <i.am.visertech@gmail.com>
2025-07-06 15:09:29 +0500

    Add files via upload

---
+++ src/constants/actions.jsx
@@ -0,0 +1,25 @@
+import DepositIcon from '@assets/icons/deposit.svg?react';
+import CryptoIcon from '@assets/icons/crypto.svg?react';
+import WithdrawIcon from '@assets/icons/withdraw.svg?react';
+import ExchangeIcon from '@assets/icons/exchange.svg?react';
+import StakingIcon from '@assets/icons/staking.svg?react';
+import NFTIcon from '@assets/icons/nft.svg?react';
+import AMLIcon from '@assets/icons/aml.svg?react';
+import CupIcon from '@assets/icons/cup.svg?react';
+import InviteIcon from '@assets/icons/invite.svg?react';
+import SupportIcon from '@assets/icons/support.svg?react';
+
+const ACTIONS = [
+  { title: 'Deposit', icon: <DepositIcon />, primary: true },
+  { title: 'Buy Crypto', icon: <CryptoIcon />, primary: true },
+  { title: 'Withdraw', icon: <WithdrawIcon /> },
+  { title: 'Exchange', icon: <ExchangeIcon /> },
+  { title: 'Staking', icon: <StakingIcon /> },
+  { title: 'NFT', icon: <NFTIcon /> },
+  { title: 'AML', icon: <AMLIcon /> },
+  { title: 'Leaders', icon: <CupIcon /> },
+  { title: 'Invite', icon: <InviteIcon /> },
+  { title: 'Support', icon: <SupportIcon /> },
+];
+
+export default ACTIONS;
